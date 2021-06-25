8e727d4be0fa0f299af4ebaa8ee50d331d4a6e06
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2021-06-26 11:31:37 +1200

    parse text

--- src/HtmlReader.Tests/ReaderTests.cs
+++ src/HtmlReader.Tests/ReaderTests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using HtmlAgilityPack;
 
 namespace HtmlReader.Tests {
     public class ReaderTests {
@@ -8,21 +9,35 @@ namespace HtmlReader.Tests {
         }
 
         [Test]
-        public void Read () {
-            Assert.AreEqual ("Some body text goes here.", new Reader ("<html><body>Some body text goes here.</html>").Read ("//body"));
+        public void Html () {
+            Assert.AreEqual ("<h1>Some body text goes here.</h1>", new Reader ("<html><body><h1>Some body text goes here.</h1></html>").Html ("//body"));
+        }
+
+        [Test]
+        public void Text() {
+            Assert.AreEqual ("Some body text goes here.", new Reader ("<html><body><h1>Some body text goes here.</h1></html>").Text("//body"));
         }
     }
 
     public class Reader {
         private readonly string html;
+        private readonly HtmlDocument document;
+        private readonly HtmlNode documentNode;
 
         public Reader(string html) {
             this.html = html;
+            this.document = new HtmlDocument();
+            this.document.LoadHtml(this.html);
+            this.documentNode = this.document.DocumentNode;
         }
-        public string Read(string path) {
+        public string Html(string path) {
             var document = new HtmlAgilityPack.HtmlDocument();
             document.LoadHtml(this.html);
-            return document.DocumentNode.SelectSingleNode(path).InnerHtml;
+            return this.document.DocumentNode.SelectSingleNode(path).InnerHtml;
+        }
+
+        public string Text(string path) {
+            return documentNode.SelectSingleNode(path).InnerText;
         }
     }
 }
