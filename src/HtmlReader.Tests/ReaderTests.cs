1cc866ffe1c361724025a8d0d87280d27136e503
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2021-06-26 12:59:15 +1200

    parse script

--- src/HtmlReader.Tests/ReaderTests.cs
+++ src/HtmlReader.Tests/ReaderTests.cs
@@ -1,21 +1,31 @@
 using NUnit.Framework;
 using HtmlAgilityPack;
+using Jurassic;
+using Jurassic.Library;
 
 namespace HtmlReader.Tests {
     public class ReaderTests {
+
+        private Reader reader;
+    
         [SetUp]
         public void Setup () {
-
+            reader = new Reader ("<html><head><script>\nwindow.givenNumber = 12;\n</script></head><body><h1>Some body text goes here.</h1></html>");
         }
 
         [Test]
         public void Html () {
-            Assert.AreEqual ("<h1>Some body text goes here.</h1>", new Reader ("<html><body><h1>Some body text goes here.</h1></html>").Html ("//body"));
+            Assert.AreEqual ("<h1>Some body text goes here.</h1>", reader.Html ("//body"));
         }
 
         [Test]
         public void Text() {
-            Assert.AreEqual ("Some body text goes here.", new Reader ("<html><body><h1>Some body text goes here.</h1></html>").Text("//body"));
+            Assert.AreEqual ("Some body text goes here.", reader.Text("//body"));
+        }
+
+        [Test]
+        public void ParseScript() {
+            Assert.AreEqual("{\"givenNumber\":12}", reader.ParseScript("//script"));
         }
     }
 
@@ -33,6 +43,13 @@ namespace HtmlReader.Tests {
 
         public string Text(string path) {
             return documentNode.SelectSingleNode(path).InnerText;
+        }
+
+        public string ParseScript(string path) {
+            var engine = new ScriptEngine();
+            var script = documentNode.SelectSingleNode(path).InnerText;
+            var parsedScript = engine.Evaluate("(function() { var window = {}; " + script + "; return window; })()");
+            return JSONObject.Stringify(engine, parsedScript) as string;
         }
     }
 }
