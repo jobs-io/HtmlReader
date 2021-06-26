fb251a855d3fe45650bc7f07a73e66cfb6b580ca
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2021-06-26 13:41:19 +1200

    extracting reader

---
+++ src/HtmlReader.Tests/Reader.cs
@@ -0,0 +1,32 @@
+using HtmlAgilityPack;
+using Jurassic;
+using Jurassic.Library;
+
+namespace HtmlReader.Tests
+{
+    public class Reader {
+        private readonly HtmlNode documentNode;
+
+        public Reader(string html) {
+            var document = new HtmlDocument();
+            document.LoadHtml(html);
+            this.documentNode = document.DocumentNode;
+        }
+        public string Html(string path) {
+            return documentNode.SelectSingleNode(path).InnerHtml;
+        }
+
+        public string Text(string path) {
+            return documentNode.SelectSingleNode(path).InnerText;
+        }
+
+        public string ParseScript(string path, string template) {
+            var engine = new ScriptEngine();
+            var script = documentNode.SelectSingleNode(path).InnerText;
+    
+            var parsedScript = engine.Evaluate(string.Format(template, script));
+    
+            return JSONObject.Stringify(engine, parsedScript) as string;
+        }
+    }
+}
--- src/HtmlReader.Tests/ReaderTests.cs
+++ src/HtmlReader.Tests/ReaderTests.cs
@@ -1,9 +1,7 @@
 using NUnit.Framework;
-using HtmlAgilityPack;
-using Jurassic;
-using Jurassic.Library;
 
-namespace HtmlReader.Tests {
+namespace HtmlReader.Tests
+{
     public class ReaderTests {
 
         private Reader reader;
@@ -27,32 +25,6 @@ namespace HtmlReader.Tests {
         public void ParseScript() {
             var template = "(function() {{ var window = {{}}; {0}; return window; }})()";
             Assert.AreEqual("{\"givenNumber\":12}", reader.ParseScript("//script", template));
-        }
-    }
-
-    public class Reader {
-        private readonly HtmlNode documentNode;
-
-        public Reader(string html) {
-            var document = new HtmlDocument();
-            document.LoadHtml(html);
-            this.documentNode = document.DocumentNode;
-        }
-        public string Html(string path) {
-            return documentNode.SelectSingleNode(path).InnerHtml;
-        }
-
-        public string Text(string path) {
-            return documentNode.SelectSingleNode(path).InnerText;
-        }
-
-        public string ParseScript(string path, string template) {
-            var engine = new ScriptEngine();
-            var script = documentNode.SelectSingleNode(path).InnerText;
-    
-            var parsedScript = engine.Evaluate(string.Format(template, script));
-    
-            return JSONObject.Stringify(engine, parsedScript) as string;
         }
     }
 }
